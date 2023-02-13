2da70f425ba86c2f1b76288254d07caf8beeafba
Danyal Imran <dan.imran97@gmail.com>
2023-02-13 16:14:50 -0500

    first commit

---
+++ controllers/home.js
@@ -0,0 +1,31 @@
+const ItemList = require('../models/itemlist')
+
+module.exports = {
+  getIndex : async (req, res) => {
+    try {
+      const items = await ItemList.find()
+      res.render("index.ejs", {itemList: items})
+    } catch (err) {
+      if (err) return res.status(500).send(err)
+    }
+  },
+  createItem: async (req, res) => {
+    const newItem = new ItemList(
+      {
+        textInput: req.body.textInput, 
+        numInput: req.body.numInput
+      }
+    )
+    try {
+      await newItem.save()
+      console.log(newItem)
+      res.redirect("/")
+    } catch(err) {
+      if (err) return res.status(500).send(err)
+      res.redirect("/")
+    }
+  },
+  deleteItem: async (req, res) => {
+    
+  }
+}
---
+++ routes/home.js
@@ -0,0 +1,11 @@
+// Handles initial GET request for the homepage
+// Handles POST method request for adding a new item
+
+const express = require("express")
+const router = express.Router()
+const homeController = require('../controllers/home')
+
+router.get('/', homeController.getIndex) //read only
+router.post('/new', homeController.createItem) // create route
+
+module.exports = router 
