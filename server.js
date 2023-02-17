869370097e0bfea554dc2f8c144228046ac83f0f
Danyal Imran <dan.imran97@gmail.com>
2023-02-17 16:05:14 -0500

    fixed delete

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // todo - Declare Variables
 const express = require("express");
-const app = express()
-const PORT = 9001
-const mongoose = require("mongoose")
+const app = express();
+const PORT = 9001;
+const mongoose = require("mongoose");
 
   // *Import functions / routes
 const connectDB = require("./config/database")
 const homeRoutes = require("./routes/home")
-const editRoutes = require("./routes/edit")
+const editRoutes = require('./routes/edit')
 
 require('dotenv').config({path: './config/.env'})
 
@@ -18,14 +18,14 @@ connectDB()
 app.set("view engine", "ejs")
 app.use(express.static('public'))
 
-// Required ti parse form POST requests - sending the data
-app.use(express.urlencoded({extended: true}));
+// Required to parse form POST requests - sending the data
+app.use(express.urlencoded({ extended: true }));
 
 
 // todo - Set Routes
-app.use('/', homeRoutes)
 app.use('/edit', editRoutes)
+app.use('/', homeRoutes)
 
 
 // todo - Start Server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}, better go catch it!`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}, better go catch it!`));
